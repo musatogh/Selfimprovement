8753e07d6eefc902311c05a058a63326896b3aba
Toghrul-code <musayevv27@gmail.com>
2021-01-04 17:15:13 +0400

    test

---
+++ Documents/Projects/Selfimprovement/cmake-build-debug/CMakeFiles/Exercises1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercises1.dir/main.c.o"
+  "Exercises1"
+  "Exercises1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercises1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
